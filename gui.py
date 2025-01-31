9f13c0b6b82f4b3526ec2eff99d1aa4845008875
rohit18basra <rohit.basra@dedalus.eu>
2025-01-31 15:06:17 +0530

    Implemented the Add feature

--- gui.py
+++ gui.py
@@ -1,10 +1,28 @@
 import functions
 import FreeSimpleGUI as sg
 
+
 label = sg.Text("Type in a To-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-do App", layout=[[label], [input_box, add_button]])
-window.read()
-window.close()
+window = sg.Window("My To-do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+
+while True:
+
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
+
+    window.close()
